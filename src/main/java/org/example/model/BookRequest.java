b4689efe28e8c8f53df7929e9d144941e70864a3
deca <idrisadejumo@gmail.com>
2025-07-10 10:12:34 +0100

    BookRequest

---
+++ src/main/java/org/example/model/BookRequest.java
@@ -0,0 +1,46 @@
+package org.example.model;
+
+public class BookRequest {
+   private User user;
+    private String bookTitle;
+    private long timeStamp;
+
+    public BookRequest(User user, String bookTitle, long timeStamp) {
+        this.user = user;
+        this.bookTitle = bookTitle;
+        this.timeStamp = timeStamp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public String toString() {
+        return "BookRequest{" +
+                "user=" + user +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", timeStamp=" + timeStamp +
+                '}';
+    }
+}
